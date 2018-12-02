64047660cbbaa85627bb29a19a4bcfe6e4b07b0f
Sameer <11097096+sameer@users.noreply.github.com>
2018-12-01 18:06:50 -0600

    Create Flask server

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, send_file
+app = Flask(__name__)
+
+@app.route('/', defaults={'_path': ''})
+@app.route("/<path:_path>")
+def aloha(_path):
+    return send_file("aloha.jpg", mimetype="image/jpg")
+
+if __name__ == '__main__':
+    app.run()
